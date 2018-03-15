b9adc25138fcb2191c7f0e76d1b945df2b7fb1f8
Timo M. Staudinger <TimoSta@users.noreply.github.com>
2018-03-15 17:29:03 -0400

    Add git log alias

--- dist/git.ps1
+++ dist/git.ps1
@@ -1,2 +1,5 @@
-function GitStatus { git status }
-Set-Alias g GitStatus
+function Git-Status { git status }
+Set-Alias g Git-Status
+
+function Git-Glg { git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all }
+Set-Alias glg Git-Glg
